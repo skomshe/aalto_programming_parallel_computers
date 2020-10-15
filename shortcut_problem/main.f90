6c9c33edfd563dcb1187a9eed76d23126d70ce8a
Sanjay Komala Sheshachala <skomshe@gmail.com>
2020-10-15 19:35:18 +0100

    Add v2 and testing

--- shortcut_problem/main.f90
+++ shortcut_problem/main.f90
@@ -6,30 +6,50 @@ USE mod_step
 IMPLICIT NONE
 
 INTEGER  (KIND = 4)                                                         ::&
-  i1, n                          
+  i1, j1, n                          
 REAL     (KIND = 8)                                                         ::&
-  st, en                  
+  PI, st, en                  
 REAL     (KIND = 8), DIMENSION(:,:), ALLOCATABLE                            ::&
   r, d 
 
-n = 2000 
+PI = 4.0D0*ATAN(1.0D0)
+  
+n = 5 
 ALLOCATE(r(n,n))
 ALLOCATE(d(n,n))
 
-d = 1.0D0
+! A very inefficient loop
 DO i1 = 1,n
-    d(i1,i1) = 0.0D0
+  DO j1 = 1,n
+    d(i1,j1) = ABS(SIN(i1*PI/n)*COS(j1*PI/n))
+  END DO
 END DO
 
 st = OMP_GET_WTIME()
 
 ! CALL step_v0(n,d,r)
 ! CALL step_omp(n,d,r) 
-CALL step_v1(n,d,r) 
+! CALL step_v1(n,d,r) 
+! CALL step_v2(n,d,r) 
 
 en = OMP_GET_WTIME()
 
 WRITE(*,'(A,E15.4,A)') 'Time = ', en-st, 'seconds'
+
+! When testing, run with n = 5 and uncomment below
+WRITE(*,*) 'Max error = ', MAXVAL(ABS(r(1,:) - &
+  (/ 0.58778525229247325D0, 0.47552825814757682D0, 0.47552825814757671D0,&
+  0.58778525229247325D0, 0.58778525229247325D0 /)))
+WRITE(*,*) 'Max error = ', MAXVAL(ABS(r(2,:) - &
+  (/ 0.95105651629515364D0, 0.58778525229247314D0, 0.58778525229247292D0,&
+  0.95105651629515364D0, 0.95105651629515364D0 /)))
+WRITE(*,*) 'Max error = ', MAXVAL(ABS(r(3,:) - &
+  (/ 0.95105651629515375D0, 0.58778525229247314D0, 0.58778525229247303D0,&
+  0.95105651629515375D0, 0.95105651629515375D0 /)))
+WRITE(*,*) 'Max error = ', MAXVAL(ABS(r(4,:) - &
+  (/ 0.58778525229247336D0, 0.47552825814757682D0, 0.47552825814757671D0,&
+  0.58778525229247336D0, 0.58778525229247336D0 /)))
+WRITE(*,*) 'Max error = ', MAXVAL(ABS(r(5,:)))
 
 DEALLOCATE(r,d)
 
--- shortcut_problem/mod_step.f90
+++ shortcut_problem/mod_step.f90
@@ -115,8 +115,78 @@ DEALLOCATE(t)
 END SUBROUTINE step_v1
 !------------------------------------------------------------------------------
 
+SUBROUTINE step_v2(n,d,r)
 
+IMPLICIT NONE
 
+INTEGER  (KIND = 4)                                                         ::&
+  n                         
+REAL     (KIND = 8), DIMENSION(:,:)                                         ::&
+  r, d 
+
+INTEGER  (KIND = 4)                                                         ::&
+  nb, na, nab, i1, j1, k1, k2, idx
+REAL     (KIND = 8)                                                         ::&
+  tmp
+REAL     (KIND = 8), DIMENSION(:), ALLOCATABLE                              ::&
+  vv
+REAL     (KIND = 8), DIMENSION(:,:), ALLOCATABLE                            ::&
+  d_pad, t_pad
+
+nb = 16
+na = (n + nb - 1)/nb
+nab = na*nb
+
+ALLOCATE(vv(nb))
+
+ALLOCATE(d_pad(nab,n))
+ALLOCATE(t_pad(n,nab))
+!$OMP PARALLEL DO DEFAULT(NONE) &
+!$OMP PRIVATE(i1,j1,tmp) &
+!$OMP SHARED(n,d,d_pad,t_pad)   
+DO i1 = 1,n
+  DO j1 = 1,n
+    tmp = d(j1,i1)
+    d_pad(j1,i1) = tmp
+    t_pad(i1,j1) = tmp
+  END DO
+END DO
+!$OMP END PARALLEL DO
+d_pad(n+1:nab,1:n) = 1.0D5
+t_pad(1:n,n+1:nab) = 1.0D5
+
+!$OMP PARALLEL DO DEFAULT(NONE) &
+!$OMP PRIVATE(i1,j1,k1,k2,idx,tmp,vv) &
+!$OMP SHARED(n,na,nb,r,d_pad,t_pad)        
+DO i1 = 1,n
+  DO j1 = 1,n
+
+    DO k1 = 1,nb
+      vv(k1) = t_pad(k1,i1) + d_pad(k1,j1)
+    END DO
+
+    DO k1 = 2,na
+      DO k2 = 1,nb
+        idx = (k1-1)*nb + k2
+        tmp = t_pad(idx,i1) + d_pad(idx,j1)
+        vv(k2) = MIN(tmp,vv(k2))
+      END DO
+    END DO
+
+    tmp = vv(1)
+    DO k1 = 2,nb
+      tmp = MIN(vv(k1),tmp)
+    END DO
+
+    r(i1,j1) = tmp
+    
+  END DO
+END DO
+!$OMP END PARALLEL DO
+
+DEALLOCATE(vv,d_pad,t_pad)
+
+END SUBROUTINE step_v2
 !------------------------------------------------------------------------------
 
 
